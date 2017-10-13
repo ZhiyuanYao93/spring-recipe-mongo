7cee0a45e3726f135f3d28699559cb4062adda34
Zhiyuan Yao <yaozyforwork@gmail.com>
2017-10-13 12:32:40 -0700

    initial commit

---
+++ src/main/java/com/zhiyuan/springrecipe/SpringrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.zhiyuan.springrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(SpringrecipeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhiyuan/springrecipe/SpringrecipeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhiyuan.springrecipe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringrecipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
